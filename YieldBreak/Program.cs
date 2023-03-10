4e70cbf5f331df7a4ab27c6f1427631e3aad96e6
Abdul Kuzhiyil <Abdul.Kuzhiyil@ii-vi.com>
2023-03-10 15:28:44 +0000

    Add project files.

---
+++ YieldBreak/Program.cs
@@ -0,0 +1,10 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        IEnumerable<int> years = Utility.GenerateRandomYears();
+
+        foreach(var y in years)
+            Console.WriteLine(y.ToString());
+    }
+}
---
+++ YieldBreak/Utility.cs
@@ -0,0 +1,13 @@
+﻿public static class Utility
+{
+    private static readonly Random random = new();
+    public static IEnumerable<int> GenerateRandomYears()
+    {
+        int year;
+        while (true)
+        {
+            year = random.Next(2000, 2023);
+            yield return year;
+        }
+    }
+}
