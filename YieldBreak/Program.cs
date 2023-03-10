a0bf304eede82b0bf689ede89eb650810c4911db
Abdul Kuzhiyil <Abdul.Kuzhiyil@ii-vi.com>
2023-03-10 15:45:23 +0000

    new method added with yield break

--- YieldBreak/Program.cs
+++ YieldBreak/Program.cs
@@ -2,9 +2,11 @@
 {
     private static void Main(string[] args)
     {
-        IEnumerable<int> years = Utility.GenerateRandomYears();
+        //  IEnumerable<int> years = Utility.GenerateRandomYears();
 
-        foreach(var y in years)
+       // IEnumerable<int> years = Utility.GenerateRandomYearsWithYieldBreak();
+
+        foreach (var y in Utility.GenerateRandomYearsWithYieldBreak())
             Console.WriteLine(y.ToString());
     }
 }
--- YieldBreak/Utility.cs
+++ YieldBreak/Utility.cs
@@ -10,4 +10,20 @@
             yield return year;
         }
     }
+
+    public static IEnumerable<int> GenerateRandomYearsWithYieldBreak()
+    {
+        int year;
+        while (true)
+        {
+            year = random.Next(1900, 2023);
+            yield return year;
+
+            if (year % 4 == 0)
+            {
+                Console.WriteLine($"{Environment.NewLine}Encountered Leap Year:{year}");
+                yield break;
+            }
+        }
+    }
 }
